bc04fedb67d8115d7ec9e623b55ed788fca70e9d
Henri Bourcereau <henri.bourcereau@gmail.com>
2011-12-23 15:22:21 +0100

    acceleration lookup

--- cryptanalyse.js
+++ cryptanalyse.js
@@ -21,11 +21,10 @@ Crypt.assoc_method_factory = function(from, to){
 
 
 Crypt.try_methods = function(dict, text){
-//  var from = ["u","l","c","e","r","a","t","i","o","n","s"];
-  var from = ["u","l","c","e","r","a", "t"];
+  var from = ["u","l","c","e","r","a","t","i","o","n","s"];
 
   checkPermutation = function(permutation){
-//    console.log("trying "+permutation);
+//    console.log('checking '+permutation);
     var method = Crypt.assoc_method_factory(from, permutation);
     return Crypt.analyse(dict, method, text); 
   };
@@ -43,11 +42,17 @@ Crypt.getMots = function(txt){
   return mots;
 };
 
-Crypt.lookup = function(dictionnaire, mot){
+Crypt.fastlookup = function(dictionnaire, mot){
+  var reg = new RegExp("^"+mot+"$", "m");
+  return (reg.exec(dictionnaire) !== null);
+};
+
+Crypt.lookup = function(dictionnaire, mot, count){
   var compteur = 0;
   var lines = dictionnaire.split("\n");
   for (var i=0, len = lines.length;i< len;i++){
     if (lines[i] === mot){
+      if (count !== true){ return 1; }
       compteur++;
     }
   }
@@ -56,9 +61,14 @@ Crypt.lookup = function(dictionnaire, mot){
 
 Crypt.analyse = function(dictionnary, method, text){
   var mots = Crypt.getMots(text);
+  //On regarde d'abord les plus longs mots, ils ont plus de chance de ne pas êtres connus du dictionnaire.
+  mots.sort(function(a,b){
+      return b.length - a.length;
+    });
   for (var i=0, len = mots.length;i< len;i++){
     var mot = mots[i];
-    if (Crypt.lookup(dictionnary, method(mot, true)) === 0){
+//    if (Crypt.lookup(dictionnary, method(mot, true)) === 0){
+    if (!Crypt.fastlookup(dictionnary, method(mot, true))){
       //At least one word not decrypted : bad method
       return false; 
     }
