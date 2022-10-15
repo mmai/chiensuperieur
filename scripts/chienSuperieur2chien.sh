57b9168a0f5f17147669916c70c5cb3e96d994d0
Henri Bourcereau <henri.bourcereau@gmail.com>
2022-10-15 21:06:57 +0200

    maj

--- scripts/chienSuperieur2chien.sh
+++ scripts/chienSuperieur2chien.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# usage : `./solutions.sh ../input/chien_superieur1.txt`
+# usage : `./chienSuperieur2chien.sh ../input/chien_superieur1.txt`
 
 nextPerm() {
   echo ${1:1}${1:0:1} # abcd => bcda
@@ -8,16 +8,32 @@ nextPerm() {
 
 INPUT=$1
 
-# BASE="ulcerations"
+# la première indication (chien_superieur1)
 BASE="ulnoatcesir"
 PERM=$BASE
 while read line; do 
   PERM=$(nextPerm $PERM)
-  echo $line | tr $BASE $PERM
+  echo $line | tr '[:upper:]' '[:lower:]' | tr $BASE $PERM
 done < $INPUT
 
-# NB_LINES=$(wc -l < $INPUT)
-# for i in $(seq 1 $NB_LINES); do 
+echo "--------"
+
+# --------------- la nouvelle première indication (chien_superieur2)
+# BASE="ulnoatcesir"
+# PERM=$BASE
 #   PERM=$(nextPerm $PERM)
-#   sed "${i}q;d" $INPUT | tr $BASE $PERM # sed 'Nq;d' file => ecrit la Nieme ligne du fichier
-# done
+#   PERM=$(nextPerm $PERM)
+# while read line; do 
+#   PERM=$(nextPerm $PERM)
+#   echo $line | tr '[:upper:]' '[:lower:]' | tr $BASE $PERM
+# done < $INPUT
+
+# ------------ nombres de Queneau
+QUENEAU_LIST=(1 2 3 5 6 9 11 14 18 23 26 29 30 33 35 39 41 49 50 53)
+COUNT=0
+while read line; do 
+  QUENEAU=${QUENEAU_LIST[$COUNT]}
+  echo $line | tr -cd '[:alpha:]' | tr '[:upper:]' '[:lower:]' | head -c $QUENEAU | tail -c 1
+  echo ""
+  COUNT=$((COUNT+1))
+done < $INPUT
